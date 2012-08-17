505057a1b28c816d27f83da937866f3b9ec38bca
Richard Pope <richard@Richards-MacBook-Air-2.local>
2012-08-17 15:09:35 +0100

    test

--- sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
+++ sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
@@ -1,3 +1,10 @@
 <?php
-	print_r($content['field_guidance_part']);
+	foreach ($content['#items'] as $entity_uri) {
+	   $items = entity_load('field_guidance_part', $entity_uri);
+	   //Now you have a variable with the entity object loaded in it, and you can do stuff. 
+	   foreach ($items as $item ) {
+	    	print render($item);
+	   }
+
+	 }
 ?>
