0368e30c488a046b2702e7bd7abcef284154863c
Richard Pope <richard@Richards-MacBook-Air-2.local>
2012-08-17 14:39:04 +0100

    test

--- sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
+++ sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
@@ -1,24 +1,3 @@
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="content"<?php print $content_attributes; ?>>
-
-		<?php
-		print 'pre';		
-		if (!empty($content['field_guidance_part'])) {
-			print 'start';
-		  foreach ($content['field_guidance_part']['#items'] as $entity_uri) {
-		    $a_field_collection_item = entity_load('field_collection_item', $entity_uri);
-
-		    foreach ($a_field_collection_item as $a_field_collection_item_object ) {
-					print render($a_field_collection_item_object->title);
-					print render($a_field_collection_item_object->content);
-		    }
-
-		  }
-
-		}
-		?>
-		
-  </div>
-</div>
-
-
+<?php
+	print render($content['field_guidance_part']); 
+?>
