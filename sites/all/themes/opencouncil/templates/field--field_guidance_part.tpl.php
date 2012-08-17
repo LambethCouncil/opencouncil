4db23f040d4754d70b2747092aa817cace8fb329
Richard Pope <richard@Richards-MacBook-Air-2.local>
2012-08-17 15:24:52 +0100

    test

--- sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
+++ sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
@@ -1,7 +1,7 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      var_dump($content);
+      render($content);
     ?>
   </div>
 </div>
