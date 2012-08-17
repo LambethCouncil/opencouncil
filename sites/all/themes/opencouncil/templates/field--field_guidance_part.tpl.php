6e594128c93a431696c3720d2c291fc5dd374639
Richard Pope <richard@Richards-MacBook-Air-2.local>
2012-08-17 14:26:25 +0100

    test

--- sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
+++ sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
@@ -1,12 +1,1 @@
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-	    if (isset($content['field_title'])) {
-	      print '<h2>' . $content['field_title'][0]['#markup'] . '</h2>';
-	    }
-	    if (isset($content['field_content'])) {
-	      	print $content['field_content'][0]['#markup']
-	     }
-    ?>
-  </div>
-</div>
+hello
