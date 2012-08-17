e6ace7f562dec49260437a368cf42f1309865c60
Richard Pope <richard@Richards-MacBook-Air-2.local>
2012-08-17 14:34:16 +0100

    test

--- sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
+++ sites/all/themes/opencouncil/templates/field--field_guidance_part.tpl.php
@@ -1,12 +1,22 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
-    <?php
-	    if (isset($content['field_title'])) {
-	      print '<h2>' . $content['field_title'][0]['#markup'] . '</h2>';
-	    }
-	    if (isset($content['field_content'])) {
-	      	print $content['field_content'][0]['#markup'];
-	     }
-    ?>
+
+		<?php
+		if (!empty($content['field_guidance_part'])) {
+		  foreach ($content['field_guidance_part']['#items'] as $entity_uri) {
+		    $a_field_collection_item = entity_load('field_collection_item', $entity_uri);
+
+		    foreach ($a_field_collection_item as $a_field_collection_item_object ) {
+					print render($a_field_collection_item_object->title);
+					print render($a_field_collection_item_object->content);
+		    }
+
+		  }
+
+		}
+		?>
+		
   </div>
 </div>
+
+
