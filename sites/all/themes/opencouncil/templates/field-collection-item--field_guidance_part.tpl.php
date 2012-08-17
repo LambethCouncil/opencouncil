d46df043ba7a4e592613608c068ed35a3745ac75
Richard Pope <richard@Richards-MacBook-Air-2.local>
2012-08-17 16:31:16 +0100

    added id to section headings

--- sites/all/themes/opencouncil/templates/field-collection-item--field_guidance_part.tpl.php
+++ sites/all/themes/opencouncil/templates/field-collection-item--field_guidance_part.tpl.php
@@ -1,7 +1,15 @@
 <div class="guide-section <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
 		<?php
-			print '<h2>' . render($content['field_title'][0]['#markup']) . '</h2>';
+		
+			// work out which number we are on (extracted from the url)
+			$parts = explode('/', $url);
+			$part_number =  $parts[count($parts) - 1];
+
+			//print the title
+			print '<h2 id="part' . $part_number . '">' . render($content['field_title'][0]['#markup']) . '</h2>';
+			
+			//print the content
 			print render($content['field_content'][0]['#markup']);
 		?>
   </div>
