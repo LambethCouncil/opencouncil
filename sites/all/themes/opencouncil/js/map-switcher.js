d02c4734b907cf31b3b98f02c37200b9038a6aed
Michael Lenahan <michael.lenahan@gmail.com>
2012-10-17 12:39:40 +0100

    map-switcher.js

--- sites/all/themes/opencouncil/js/map-switcher.js
+++ sites/all/themes/opencouncil/js/map-switcher.js
@@ -5,8 +5,8 @@ Drupal.behaviors.yourfunction = {
         // http://stackoverflow.com/questions/1279957/how-to-move-an-element-into-another-element
         // NOTE: #block-block-1 is hard-coded.
         // commented out ml 2012-10-17 - revert to usual switcher.
-        // $('.layersDiv .dataLayersDiv').appendTo('#block-block-1');
-        // $('.layersDiv .dataLbl').hide();
+        $('.layersDiv .dataLayersDiv').appendTo('#block-block-1');
+        $('.layersDiv .dataLbl').hide();
       });
     }
   }
