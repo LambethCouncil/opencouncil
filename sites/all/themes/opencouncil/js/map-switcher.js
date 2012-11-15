b2af7c9027c5a00062ab09b8ad4f57031dcb722d
Michael Lenahan <michael.lenahan@gmail.com>
2012-11-15 14:30:46 +0000

    remove jQuery that puts map-switcher checkboxes below map

--- sites/all/themes/opencouncil/js/map-switcher.js
+++ sites/all/themes/opencouncil/js/map-switcher.js
@@ -3,17 +3,6 @@
     attach: function(context) {
       $(document).ready(function() {
 
-        // http://stackoverflow.com/questions/1279957/how-to-move-an-element-into-another-element
-        // NOTE: #block-block-1 is hard-coded.
-        // commented out ml 2012-10-17 - revert to usual switcher.
-        //
-        // UNCOMMENT THESE NEXT TWO LINES TO PUT THE SWITCHER UNDER THE MAP
-        //
-        $('.layersDiv .dataLayersDiv').appendTo('#block-block-1');
-        $('.layersDiv .dataLbl').hide();
-        //
-        //
-        
         $('#map_center_button').click(function(e) {
 
           // Get value of the textfield
