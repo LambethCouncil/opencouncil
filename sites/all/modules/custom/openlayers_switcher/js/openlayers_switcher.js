193eeceb1c8f0ef6230fdd4dac328470284b728f
Michael Lenahan <michael.lenahan@gmail.com>
2012-11-15 14:28:14 +0000

    openlayers_switcher

---
+++ sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
@@ -0,0 +1,73 @@
+(function ($) {
+  Drupal.behaviors.openlayers_switcher = {
+    attach: function(context, settings) {
+      $(document).ready(function() {
+        $('#map-switcher-block--food-growing-projects').click(function(e) {
+          var layername = 'food-growing-projects';
+          var ol = $('.openlayers-map').data('openlayers');
+          _openlayers_switcher_show_layer(ol, layername);
+        });
+      });
+    }
+  }
+}(jQuery));
+
+function _openlayers_switcher_show_layer(ol, layername) {
+  var layers = ol.openlayers.layers;
+  var layer_index = -1;
+  var currently_visible = false;
+  var visibility_known = false;
+  for (var i = 0; i < layers.length; ++i) {
+    if (!layers[i].isBaseLayer) {
+      if (layers[i].name == layername) {
+        if (layer_index < 0) {
+        // commented out - for some reason this gets called twice.
+        // so first time the layer gets shown, then gets hidden again!
+        //
+        //
+        //TRY THIS
+        //http://stackoverflow.com/questions/8238599/jquery-click-fires-twice-when-clicking-on-label
+        //
+        //
+        //
+        //console.log(layers[i]);
+        // alert(i);
+        // alert(layername);
+        // get current visibility
+        currently_visible = layers[i].visibility;
+        layer_index = i;
+        //alert(currently_visible);
+        }
+      }
+    }
+  }
+  // toggle visibility
+  // alert(currently_visible);
+  // alert(layer_index);
+  // if (currently_visible) {
+    // layers[layer_index].setVisibility(false);
+  // }
+  // else {
+    layers[layer_index].setVisibility(true);
+  // }
+}
+
+function _openlayers_switcher_show_all_layers(ol) {
+  var layers = ol.openlayers.layers;
+  for (var i = 0; i < layers.length; ++i) {
+    if (!layers[i].isBaseLayer) {
+      alert('id = ' + layers[i].id);
+      alert('name = ' + layers[i].name);
+      layers[i].setVisibility(true);
+    }
+  }
+}
+
+function _hide_all_layers() {
+  var layers = ol.openlayers.layers;
+  for (var i = 0; i < layers.length; ++i) {
+    if (!layers[i].isBaseLayer) {
+      layers[i].setVisibility(false);
+    }
+  }
+}
