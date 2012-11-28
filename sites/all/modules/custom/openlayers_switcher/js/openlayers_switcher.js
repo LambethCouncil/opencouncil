3bbbac30dc758a717696cd64bff7935209938f10
Michael Lenahan <michael.lenahan@gmail.com>
2012-11-28 16:59:18 +0100

    reverting to jQuery appendTo to move the switcher checkboxes below the map. Note - this method does not work with openlayers 2.12 so using 2.11 instead

--- sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
+++ sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
@@ -3,16 +3,12 @@
     attach: function(context, settings) {
       $(document).ready(function() {
         // http://stackoverflow.com/questions/1279957/how-to-move-an-element-into-another-element
-        // NOTE: #block-block-1 is hard-coded.
-        // commented out ml 2012-10-17 - revert to usual switcher.
-        //
-        // UNCOMMENT THESE NEXT TWO LINES TO PUT THE SWITCHER UNDER THE MAP
-        //
-          // $('.layersDiv .dataLayersDiv').appendTo('#openlayers-switcher-block-lhs');
-          // $('.layersDiv .dataLbl').hide();
-        //
-        //
-        
+        // This uses jQuery to move the layer switcher checkboxes from the lhs
+        // switcher block to below the map.
+        if ($('.layersDiv .dataLayersDiv').length) {
+          $('.layersDiv .dataLayersDiv').appendTo('#openlayers-switcher-block');
+          $('.layersDiv .dataLbl').hide();
+        }
       });
     }
   }
