4c64151fd4bbe8d16ad2f23ddf5fe5ecf41ed483
Michael Lenahan <michael.lenahan@gmail.com>
2012-11-28 14:35:36 +0100

    commit prior to switch branch to map-switcher-jquery

--- sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
+++ sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
@@ -2,30 +2,17 @@
   Drupal.behaviors.openlayers_switcher = {
     attach: function(context, settings) {
       $(document).ready(function() {
-        // see http://drupal.org/node/756722
-        // jQuery Once method for applying JavaScript behaviors once
-        // $('[id^=openlayers-switcher-block--]').once('click', function() {
-        $('[id^=openlayers-switcher-block--]').click(function(e) {
-          // console.log($(this));
-          var layername = e.target.id.substring(27);
-          var ol = $('.openlayers-map').data('openlayers');
-          var layers = ol.openlayers.getLayersByName(layername);
-
-          checked = $('[name=' + layername + ']')[0].checked;
-          console.log('checked=' + checked)
-          visibility = layers[0].visibility;
-          console.log('visibility=' + visibility);
-
-          layers[0].setVisibility(true);
-          // this doesn't work
-          //if (layers[0].visibility) {
-            //layers[0].setVisibility(false);
-          //}
-          //else {
-            //layers[0].setVisibility(true);
-          //}
-          
-        });
+        // http://stackoverflow.com/questions/1279957/how-to-move-an-element-into-another-element
+        // NOTE: #block-block-1 is hard-coded.
+        // commented out ml 2012-10-17 - revert to usual switcher.
+        //
+        // UNCOMMENT THESE NEXT TWO LINES TO PUT THE SWITCHER UNDER THE MAP
+        //
+          // $('.layersDiv .dataLayersDiv').appendTo('#openlayers-switcher-block-lhs');
+          // $('.layersDiv .dataLbl').hide();
+        //
+        //
+        
       });
     }
   }
