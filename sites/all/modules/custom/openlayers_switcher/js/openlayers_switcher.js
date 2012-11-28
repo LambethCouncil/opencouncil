2dc46ee7596242a650826b43d28259eeeb3268cc
Michael Lenahan <michael.lenahan@gmail.com>
2012-11-28 13:10:12 +0100

    openlayers_switcher on master before switching to new branch with older code

--- sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
+++ sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
@@ -2,21 +2,29 @@
   Drupal.behaviors.openlayers_switcher = {
     attach: function(context, settings) {
       $(document).ready(function() {
+        // see http://drupal.org/node/756722
+        // jQuery Once method for applying JavaScript behaviors once
+        // $('[id^=openlayers-switcher-block--]').once('click', function() {
         $('[id^=openlayers-switcher-block--]').click(function(e) {
-          $(this).toggleClass('active');
-          // 27 is the length of 'openlayers-switcher-block--'
-          // layername is the string following that.
+          // console.log($(this));
           var layername = e.target.id.substring(27);
           var ol = $('.openlayers-map').data('openlayers');
           var layers = ol.openlayers.getLayersByName(layername);
+
+          checked = $('[name=' + layername + ']')[0].checked;
+          console.log('checked=' + checked)
+          visibility = layers[0].visibility;
+          console.log('visibility=' + visibility);
+
           layers[0].setVisibility(true);
           // this doesn't work
-          // if (layers[0].visibility) {
-            // layers[0].setVisibility(false);
-          // }
-          // else {
-            // layers[0].setVisibility(true);
-          // }
+          //if (layers[0].visibility) {
+            //layers[0].setVisibility(false);
+          //}
+          //else {
+            //layers[0].setVisibility(true);
+          //}
+          
         });
       });
     }
