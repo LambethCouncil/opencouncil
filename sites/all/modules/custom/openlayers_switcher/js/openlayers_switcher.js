c9f478d05bc14679bdaa53ff5407721abbf86560
Michael Lenahan <michael.lenahan@gmail.com>
2012-11-30 11:57:36 +0000

    jQuery to move switchers from lhs switcher widget now in openlayers_switcher.js within the module

--- sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
+++ sites/all/modules/custom/openlayers_switcher/js/openlayers_switcher.js
@@ -2,12 +2,14 @@
   Drupal.behaviors.openlayers_switcher = {
     attach: function(context, settings) {
       $(document).ready(function() {
-        // http://stackoverflow.com/questions/1279957/how-to-move-an-element-into-another-element
-        // This uses jQuery to move the layer switcher checkboxes from the lhs
-        // switcher block to below the map.
+        // Move the layer switcher checkboxes and labels out of the
+        // left-hand-side OpenLayers switcher widget, and put the content of
+        // .dataLayersDiv inside the openlayers-switcher-block defined by the
+        // module.
         if ($('.layersDiv .dataLayersDiv').length) {
           $('.layersDiv .dataLayersDiv').appendTo('#openlayers-switcher-block');
-        }
+          $('.layersDiv .dataLbl').hide();
+        }        
       });
     }
   }
