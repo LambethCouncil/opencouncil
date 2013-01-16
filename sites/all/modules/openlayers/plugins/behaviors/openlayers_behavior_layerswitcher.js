d90771f1b9c1eb565a944419cc77e8cd061bb079
Michael Lenahan <michael.lenahan@gmail.com>
2013-01-16 12:22:29 +0000

    openlayers 7.x-2.0-beta3

--- sites/all/modules/openlayers/plugins/behaviors/openlayers_behavior_layerswitcher.js
+++ sites/all/modules/openlayers/plugins/behaviors/openlayers_behavior_layerswitcher.js
@@ -8,8 +8,12 @@
  */
 Drupal.openlayers.addBehavior('openlayers_behavior_layerswitcher', function (data, options) {
   options.ascending = !! options.ascending;
-  Drupal.openlayers.addControl(data.openlayers, 'LayerSwitcher', options);
-
+// lambeth.coop
+console.log(options);
+options['div'] = OpenLayers.Util.getElement('openlayers-switcher-block');
+Drupal.openlayers.addControl(data.openlayers, 'LayerSwitcher', options);
+console.log(options);
+//
   // Maximize if needed.
   if (!! options.maximizeDefault == true) {
     data.openlayers.getControlsByClass('OpenLayers.Control.LayerSwitcher')[0].maximizeControl();
