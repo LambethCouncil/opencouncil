5edbadee8c6115941296b9f9dc5e5e3518f13cbe
Michael Lenahan <michael.lenahan@gmail.com>
2013-01-16 12:36:34 +0000

    go back to original openlayers_behavior_layerswitcher.js in openlayers module

--- sites/all/modules/openlayers/plugins/behaviors/openlayers_behavior_layerswitcher.js
+++ sites/all/modules/openlayers/plugins/behaviors/openlayers_behavior_layerswitcher.js
@@ -8,12 +8,8 @@
  */
 Drupal.openlayers.addBehavior('openlayers_behavior_layerswitcher', function (data, options) {
   options.ascending = !! options.ascending;
-// lambeth.coop
-console.log(options);
-options['div'] = OpenLayers.Util.getElement('openlayers-switcher-block');
-Drupal.openlayers.addControl(data.openlayers, 'LayerSwitcher', options);
-console.log(options);
-//
+  Drupal.openlayers.addControl(data.openlayers, 'LayerSwitcher', options);
+
   // Maximize if needed.
   if (!! options.maximizeDefault == true) {
     data.openlayers.getControlsByClass('OpenLayers.Control.LayerSwitcher')[0].maximizeControl();
