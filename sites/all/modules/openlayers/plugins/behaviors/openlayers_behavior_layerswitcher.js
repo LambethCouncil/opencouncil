3af2597272940d778a3c85d7d2e31e867b3d7e7c
Michael Lenahan <michael.lenahan@gmail.com>
2013-01-16 13:39:09 +0000

    dirty hack d.o/node/1475316

--- sites/all/modules/openlayers/plugins/behaviors/openlayers_behavior_layerswitcher.js
+++ sites/all/modules/openlayers/plugins/behaviors/openlayers_behavior_layerswitcher.js
@@ -8,6 +8,11 @@
  */
 Drupal.openlayers.addBehavior('openlayers_behavior_layerswitcher', function (data, options) {
   options.ascending = !! options.ascending;
+
+  // lambeth.coop - move layerswitcher outside map to openlayers-switcher-block
+  // div
+  // http://drupal.org/node/1475316
+  options['div'] = OpenLayers.Util.getElement('openlayers-switcher-block');
   Drupal.openlayers.addControl(data.openlayers, 'LayerSwitcher', options);
 
   // Maximize if needed.
