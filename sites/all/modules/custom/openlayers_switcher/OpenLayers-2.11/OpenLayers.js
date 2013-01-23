ebb9f8e4ea66f439425ce72f2552e5585fddab48
Michael Lenahan <michael.lenahan@gmail.com>
2013-01-23 12:30:03 +0000

    added comments to modified OpenLayers.js

--- sites/all/modules/custom/openlayers_switcher/OpenLayers-2.11/OpenLayers.js
+++ sites/all/modules/custom/openlayers_switcher/OpenLayers-2.11/OpenLayers.js
@@ -2814,16 +2814,39 @@ var layers=this.map.layers.slice();if(!this.ascending){layers.reverse();}
 for(var i=0,len=layers.length;i<len;i++){var layer=layers[i];var baseLayer=layer.isBaseLayer;if(layer.displayInLayerSwitcher){if(baseLayer){containsBaseLayers=true;}else{containsOverlays=true;}
 var checked=(baseLayer)?(layer==this.map.baseLayer):layer.getVisibility();var inputElem=document.createElement("input");inputElem.id=this.id+"_input_"+layer.name;inputElem.name=(baseLayer)?this.id+"_baseLayers":layer.name;inputElem.type=(baseLayer)?"radio":"checkbox";inputElem.value=layer.name;inputElem.checked=checked;inputElem.defaultChecked=checked;if(!baseLayer&&!layer.inRange){inputElem.disabled=true;}
 
-// ML
+// lambeth.coop
 var humanLayerName=layer.name.replace(/-/g, ' ');
+//
 
-var context={'inputElem':inputElem,'layer':layer,'layerSwitcher':this};OpenLayers.Event.observe(inputElem,"mouseup",OpenLayers.Function.bindAsEventListener(this.onInputClick,context));var aSwitcherBlock=document.createElement("a");var iconSwitcherSpan=document.createElement("span");var labelSpan=document.createElement("span");
+var context={'inputElem':inputElem,'layer':layer,'layerSwitcher':this};OpenLayers.Event.observe(inputElem,"mouseup",OpenLayers.Function.bindAsEventListener(this.onInputClick,context));
 
-OpenLayers.Element.addClass(aSwitcherBlock,"openlayers-switcher-block--"+layer.name);OpenLayers.Element.addClass(iconSwitcherSpan,"icon-switcher");OpenLayers.Element.addClass(labelSpan,"switcher-text");if(!baseLayer&&!layer.inRange){labelSpan.style.color="gray";}
+// lambeth.coop
+// create new helper elements for styling
+var aSwitcherBlock=document.createElement("a");
+var iconSwitcherSpan=document.createElement("span");
+var labelSpan=document.createElement("span");
+// add classes for styling
+OpenLayers.Element.addClass(aSwitcherBlock,"openlayers-switcher-block--"+layer.name);
+OpenLayers.Element.addClass(iconSwitcherSpan,"icon-switcher");
+OpenLayers.Element.addClass(labelSpan,"switcher-text");
+//
 
+if(!baseLayer&&!layer.inRange){labelSpan.style.color="gray";}
+
+// set label to have spaces instead of dashes
 labelSpan.innerHTML=humanLayerName;
+//
 
-labelSpan.style.verticalAlign=(baseLayer)?"bottom":"baseline";OpenLayers.Event.observe(aSwitcherBlock,"click",OpenLayers.Function.bindAsEventListener(this.onInputClick,context));var br=document.createElement("br");var groupArray=(baseLayer)?this.baseLayers:this.dataLayers;groupArray.push({'layer':layer,'inputElem':inputElem,'labelSpan':labelSpan});var groupDiv=(baseLayer)?this.baseLayersDiv:this.dataLayersDiv;groupDiv.appendChild(aSwitcherBlock);aSwitcherBlock.appendChild(iconSwitcherSpan);aSwitcherBlock.appendChild(labelSpan);}}
+labelSpan.style.verticalAlign=(baseLayer)?"bottom":"baseline";
+
+// bind the click event to the aSwitcherBlock a element
+OpenLayers.Event.observe(aSwitcherBlock,"click",OpenLayers.Function.bindAsEventListener(this.onInputClick,context));
+
+var br=document.createElement("br");var groupArray=(baseLayer)?this.baseLayers:this.dataLayers;groupArray.push({'layer':layer,'inputElem':inputElem,'labelSpan':labelSpan});var groupDiv=(baseLayer)?this.baseLayersDiv:this.dataLayersDiv;
+
+// set the structure of the html elements
+groupDiv.appendChild(aSwitcherBlock);aSwitcherBlock.appendChild(iconSwitcherSpan);aSwitcherBlock.appendChild(labelSpan);}}
+
 this.dataLbl.style.display=(containsOverlays)?"":"none";this.baseLbl.style.display=(containsBaseLayers)?"":"none";return this.div;},onInputClick:function(e){if(!this.inputElem.disabled){if(this.inputElem.type=="radio"){this.inputElem.checked=true;this.layer.map.setBaseLayer(this.layer);}else{this.inputElem.checked=!this.inputElem.checked;this.layerSwitcher.updateMap();}}
 OpenLayers.Event.stop(e);},onLayerClick:function(e){this.updateMap();},updateMap:function(){for(var i=0,len=this.baseLayers.length;i<len;i++){var layerEntry=this.baseLayers[i];if(layerEntry.inputElem.checked){this.map.setBaseLayer(layerEntry.layer,false);}}
 for(var i=0,len=this.dataLayers.length;i<len;i++){var layerEntry=this.dataLayers[i];layerEntry.layer.setVisibility(layerEntry.inputElem.checked);}},maximizeControl:function(e){this.div.style.width="";this.div.style.height="";this.showControls(false);if(e!=null){OpenLayers.Event.stop(e);}},minimizeControl:function(e){this.div.style.width="0px";this.div.style.height="0px";this.showControls(true);if(e!=null){OpenLayers.Event.stop(e);}},showControls:function(minimize){this.maximizeDiv.style.display=minimize?"":"none";this.minimizeDiv.style.display=minimize?"none":"";this.layersDiv.style.display=minimize?"none":"";},loadContents:function(){OpenLayers.Event.observe(this.div,"mouseup",OpenLayers.Function.bindAsEventListener(this.mouseUp,this));OpenLayers.Event.observe(this.div,"click",this.ignoreEvent);OpenLayers.Event.observe(this.div,"mousedown",OpenLayers.Function.bindAsEventListener(this.mouseDown,this));OpenLayers.Event.observe(this.div,"dblclick",this.ignoreEvent);this.layersDiv=document.createElement("div");this.layersDiv.id=this.id+"_layersDiv";OpenLayers.Element.addClass(this.layersDiv,"layersDiv");this.baseLbl=document.createElement("div");this.baseLbl.innerHTML=OpenLayers.i18n("Base Layer");OpenLayers.Element.addClass(this.baseLbl,"baseLbl");this.baseLayersDiv=document.createElement("div");OpenLayers.Element.addClass(this.baseLayersDiv,"baseLayersDiv");this.dataLbl=document.createElement("div");this.dataLbl.innerHTML=OpenLayers.i18n("Overlays");OpenLayers.Element.addClass(this.dataLbl,"dataLbl");this.dataLayersDiv=document.createElement("div");OpenLayers.Element.addClass(this.dataLayersDiv,"dataLayersDiv");if(this.ascending){this.layersDiv.appendChild(this.baseLbl);this.layersDiv.appendChild(this.baseLayersDiv);this.layersDiv.appendChild(this.dataLbl);this.layersDiv.appendChild(this.dataLayersDiv);}else{this.layersDiv.appendChild(this.dataLbl);this.layersDiv.appendChild(this.dataLayersDiv);this.layersDiv.appendChild(this.baseLbl);this.layersDiv.appendChild(this.baseLayersDiv);}
